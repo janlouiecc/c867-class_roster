217fe59c3c1a3586124756efca432accab7afc9b
jnlcst <70475659+jnlcst@users.noreply.github.com>
2021-09-17 01:43:26 +1000

    Add files via upload

---
+++ degree.h
@@ -0,0 +1,11 @@
+#ifndef DEGREE_H
+#define DEGREE_H
+
+enum class DegreeProgram
+{
+	SECURITY,
+	NETWORK,
+	SOFTWARE
+};
+
+#endif
---
+++ roster.cpp
@@ -0,0 +1,129 @@
+#include "roster.h"
+
+void Roster::parseAndAdd(string studentData)
+{
+	DegreeProgram degreeProgram = DegreeProgram::SOFTWARE;
+	int rightSide = studentData.find(',');
+	string studentID = studentData.substr(0, rightSide);
+
+	int leftSide = rightSide + 1;
+	rightSide = studentData.find(',', leftSide);
+	string firstName = studentData.substr(leftSide, rightSide - leftSide);
+
+	leftSide = rightSide + 1;
+	rightSide = studentData.find(',', leftSide);
+	string lastName = studentData.substr(leftSide, rightSide - leftSide);
+
+	leftSide = rightSide + 1;
+	rightSide = studentData.find(',', leftSide);
+	string emailAddress = studentData.substr(leftSide, rightSide - leftSide);
+
+	leftSide = rightSide + 1;
+	rightSide = studentData.find(',', leftSide);
+	int age = stoi(studentData.substr(leftSide, rightSide - leftSide));
+
+	leftSide = rightSide + 1;
+	rightSide = studentData.find(',', leftSide);
+	int daysToCompleteCourse1 = stoi(studentData.substr(leftSide, rightSide - leftSide));
+
+	leftSide = rightSide + 1;
+	rightSide = studentData.find(',', leftSide);
+	int daysToCompleteCourse2 = stoi(studentData.substr(leftSide, rightSide - leftSide)); 
+	
+	leftSide = rightSide + 1;
+	rightSide = studentData.find(',', leftSide);
+	int daysToCompleteCourse3 = stoi(studentData.substr(leftSide, rightSide - leftSide));
+
+	leftSide = rightSide + 1;
+	rightSide = studentData.find(',', leftSide);
+
+	if (studentData.substr(leftSide, rightSide - leftSide) == "SECURITY")
+	{
+		degreeProgram = DegreeProgram::SECURITY;
+	}
+	else if (studentData.substr(leftSide, rightSide - leftSide) == "NETWORK")
+	{
+		degreeProgram = DegreeProgram::NETWORK;
+	}
+	else if (studentData.substr(leftSide, rightSide - leftSide) == "SOFTWARE")
+	{
+		degreeProgram = DegreeProgram::SOFTWARE;
+	}
+
+	add(studentID, firstName, lastName, emailAddress, age, daysToCompleteCourse1, daysToCompleteCourse2, daysToCompleteCourse3, degreeProgram);
+}
+
+void Roster::add(string studentID, string firstName, string lastName, string emailAddress, int age, int daysToCompleteCourse1, int daysToCompleteCourse2, int daysToCompleteCourse3, DegreeProgram degreeProgram)
+{
+	int daysToCompleteCourse[] = { daysToCompleteCourse1, daysToCompleteCourse2, daysToCompleteCourse3 };
+	lastIndex++;
+	classRosterArray[lastIndex] = new Student(studentID, firstName, lastName, emailAddress, age, daysToCompleteCourse, degreeProgram);
+}
+
+void Roster::printAll()
+{
+	std::cout << "Displaying all students:" << std::endl;
+	for (int i = 0; i < 5; i++)
+	{
+		classRosterArray[i]->Print();
+	}
+	std::cout << std::endl;
+}
+
+void Roster::printInvalidEmails()
+{
+	std::cout << "Displaying Invalid E-mails:" << std::endl;
+
+	for (int i = 0; i < 5; i++)
+	{
+		string email = classRosterArray[i]->getEmailAddress();
+		if (email.find('@') == string::npos || email.find(' ') != string::npos || email.find(".") == string::npos)
+		{
+			std::cout << classRosterArray[i]->getEmailAddress() << " is INVALID.";
+			std::cout << std::endl;
+		}
+	}
+	std::cout << std::endl;
+}
+
+void Roster::printByDegreeProgram(DegreeProgram degreeProgram)
+{
+	switch (degreeProgram)
+	{
+	case DegreeProgram::NETWORK: std::cout << "Displaying students in the NETWORK degree program:" << std::endl; break;
+	case DegreeProgram::SOFTWARE: std::cout << "Displaying students in the SOFTWARE degree program:" << std::endl; break;
+	case DegreeProgram::SECURITY: std::cout << "Displaying students in the SECURITY degree program:" << std::endl; break;
+	}
+
+
+	for (int i = 0; i < 5; i++)
+	{
+		if (classRosterArray[i]->getDegreeProgram() == degreeProgram)
+		{
+			classRosterArray[i]->Print();
+		}
+	}
+	std::cout << std::endl;
+}
+
+void Roster::remove(string studentID)
+{
+	// You left off here.
+}
+
+void Roster::printAverageDaysInCourse(string studentID)
+{
+	int a, b, c;
+	for (int i = 0; i < 5; i++)
+	{
+		if (studentID == classRosterArray[i]->getStudentID())
+		{
+			a = *(classRosterArray[i]->getDaysToCompleteCourse());
+			b = *(classRosterArray[i]->getDaysToCompleteCourse() + 1);
+			c = *(classRosterArray[i]->getDaysToCompleteCourse() + 2);
+		}
+	}
+	std::cout << "Student ID: " << studentID << ", average days left in a course - " << (a + b + c) / 3 << " days." << std::endl;
+}
+
+Roster::~Roster() = default;
